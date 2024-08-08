010e41396ed7c321ac2656c79a9cb583f82677f0
Galang Bilal <galang.bilal@sahassa.co.id>
2024-08-08 17:33:53 +0700

    Add project files.

---
+++ DataStructure_StackQueue.cs
@@ -0,0 +1,54 @@
+﻿public class DataStructure_StackQueue
+{
+    private Queue<int> q1;
+    private Queue<int> q2;
+
+    public DataStructure_StackQueue()
+    {
+        q1 = new Queue<int>();
+        q2 = new Queue<int>();
+    }
+
+    public void Push(int value)
+    {
+        q1.Enqueue(value);
+    }
+
+    public int Pop()
+    {
+        if (q1.Count == 0)
+        {
+            throw new InvalidOperationException("Stack is empty");
+        }
+
+        while (q1.Count > 1)
+        {
+            q2.Enqueue(q1.Dequeue());
+        }
+
+        int top = q1.Dequeue();
+
+        // swap the queue
+        (q2, q1) = (q1, q2);
+        return top;
+    }
+
+    public int Top()
+    {
+        if (q1.Count == 0)
+        {
+            throw new InvalidOperationException("Stack is empty");
+        }
+
+        while (q1.Count > 1)
+        {
+            q2.Enqueue(q1.Dequeue());
+        }
+
+        int top = q1.Peek();
+
+        // swap the queue
+        (q2, q1) = (q1, q2);
+        return top;
+    }
+}
